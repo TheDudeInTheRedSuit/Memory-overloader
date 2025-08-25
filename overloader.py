e0342823d6b9be75516dc6a7f21ba420257eb7cc
TheGuyInTheRedSuit <kylelsturrock@gmail.com>
2025-08-25 12:11:23 +0200

    Update overloader.py

--- overloader.py
+++ overloader.py
@@ -2,3 +2,10 @@ def createfile():
   with open("myfile.txt", "w") as f:
       for i in range(1000000)
       f.write("REHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHE\n")
+
+nooffiles = input("how many 200MB files do you want to make")
+nooffiles = int(nooffiles)
+
+for j in range(noofiles)
+  createfile()
+
